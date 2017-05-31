247a519c6c48e6ccde99576fc80e88a9da58560b
Andrew Ortega <andrewrortega@chicagobooth.edu>
2017-05-31 01:53:00 -0500

    update 2 need to reseed the db
    need to reseed the db

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,6 @@
 Rails.application.routes.draw do
   root "photos#index"
 
-
-  # Routes for the Comment resource:
-  # CREATE
-  get "/users/new", :controller => "users", :action => "new"
-  post "/create_comment", :controller => "users", :action => "create"
-
-  # READ
-  get "/comments", :controller => "users", :action => "index"
-  get "/comments/:id", :controller => "users", :action => "show"
-
-  # UPDATE
-  get "/comments/:id/edit", :controller => "comments", :action => "edit"
-  post "/update_comment/:id", :controller => "comments", :action => "update"
-
-  # DELETE
-  get "/delete_comment/:id", :controller => "comments", :action => "destroy"
-  #------------------------------
-
-
   # Routes for the Comment resource:
   # CREATE
   get "/comments/new", :controller => "comments", :action => "new"
@@ -72,5 +53,12 @@ Rails.application.routes.draw do
   #------------------------------
 
   devise_for :users
+
+  # Routes for the User resource:
+
+  # READ
+  get "/users", :controller => "users", :action => "index"
+  get "/users/:id", :controller => "users", :action => "show"
+  #------------------------------
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
